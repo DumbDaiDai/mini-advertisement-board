1d96eb917f8a80dfc9af3f4e9228658312f271ed
DumbDaiDai <3234627960@qq.com>
2025-12-10 02:14:35 +0800

    fix: 修改lint问题

--- app/components/template/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div :class="styles.container">
-
-  </div>
-</template>
-
-<script setup lang="ts">
-import { default as styles } from "./index.module.css";
-</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div :class="$style.page">
     <div :class="$style.headBar">
